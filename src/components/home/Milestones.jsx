2144dd40e8db39aa3080d6f15243155ce33e4ab1
Shyam M <64887333+Shyam-tvr@users.noreply.github.com>
2025-09-07 19:38:00 +0530

    Update Milestones.jsx

--- src/components/home/Milestones.jsx
+++ src/components/home/Milestones.jsx
@@ -3,19 +3,19 @@ import Slider from "./Slider";
 
 const sliderContent = [
   {
-    image: "/images/home/Milestone1.png",
+    image: "/images/home/milestone1.png",
     title: "Bandicoot 2.0 Launched in the presence of Hon'ble Prime Minister",
     description:
       "A powerful validation of our mission, Bandicoot 2.0 was launched in the presence of Hon'ble Prime Minister Shri Narendra Modi and UN Secretary-General António Guterres at the Mahatma Gandhi International Sanitation Convention in 2018. This marked a national endorsement of using indigenous technology for the Swachh Bharat Mission.",
   },
   {
-    image: "/images/home/Milestone2.png",
+    image: "/images/home/milestone2.png",
     title: "Uttar Pradesh: A New Era of Worker Safety",
     description:
       "UP Chief Minister Shri Yogi Adityanath launched Bandicoot for the end of manual scavenging. This milestone reflects the government’s commitment to worker safety and dignity by replacing hazardous manual methods with robotic technology.",
   },
   {
-    image: "/images/home/Milestone3.png",
+    image: "/images/home/milestone3.png",
     title: "Kerala’s Pioneering Commitment: Statewide Deployment",
     description:
       "Hon’ble Chief Minister of Kerala, Shri Pinarayi Vijayan, inaugurated Bandicoot Robot in February 2018, marking the beginning of a new era in sanitation. He also signed a landmark MOU for the statewide deployment of Bandicoot robots, reaffirming Kerala’s commitment to eradicating manual scavenging and restoring safety and dignity for sanitation workers.",
